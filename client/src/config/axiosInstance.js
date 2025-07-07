b25c952eb0431b01b51b2dbaa3e86cc02e1118ec
utpal-kumar-08 <um697893@gmail.com>
2025-07-07 07:04:50 +0530

    Delete client directory

--- client/src/config/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL="http://localhost:6600/api/"
-const axiosInstance=axios.create()
-
-axiosInstance.defaults.baseURL=BASE_URL;
-
-export default axiosInstance;
